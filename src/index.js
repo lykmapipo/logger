553b775801ae8cee6ab896e5e581b1d8603be3b3
lykmapipo <lallyelias87@gmail.com>
2019-04-10 17:03:28 +0300

    feat: add createLogger and disposeLogger utilities

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
+import { createLogger as buildLogger, transports } from 'winston';
+
+// ref logger instance
+let logger;
+
+/**
+ * @function logger
+ * @name logger
+ * @description create logger instance if not exists
+ * @param {Object} [customLogger] custom lgger to use
+ * @return {Object} A new instance of of logger
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @private
+ * @example
+ *
+ * import { createLogger } from '@lykmapipo/logger';
+ * const logger = createLogger();
+ */
+export const createLogger = customLogger => {
+  // create logger
+  if (!logger) {
+    // use custom logger
+    if (customLogger) {
+      logger = customLogger;
+    }
+    // use library logger
+    else {
+      logger = buildLogger({
+        level: 'silly',
+        transports: [new transports.Console()],
+      });
+      // set custom looger id
+      logger.id = Date.now();
+    }
+  }
+
+  // return created logger
+  return logger;
+};
+
+/**
+ * @function disposeLogger
+ * @name disposeLogger
+ * @description reset current logger instance in use.
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ * import { disposeLogger } from '@lykmapipo/logger';
+ * const logger = disposeLogger();
+ * //=> null
+ */
+export const disposeLogger = () => {
+  logger = null;
+  return logger;
+};
+
 export const error = () => {};
 export const warn = () => {};
 export const info = () => {};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,52 @@
 import { expect } from 'chai';
-import { error, warn, info, verbose, debug, silly } from '../src/index';
+import {
+  createLogger,
+  disposeLogger,
+  error,
+  warn,
+  info,
+  verbose,
+  debug,
+  silly,
+} from '../src/index';
 
 describe('logger', () => {
+  beforeEach(() => {
+    disposeLogger();
+  });
+
   it('should expose log helpers', () => {
+    expect(createLogger).to.exist.and.to.be.a('function');
+    expect(disposeLogger).to.exist.and.to.be.a('function');
     expect(error).to.exist.and.to.be.a('function');
     expect(warn).to.exist.and.to.be.a('function');
     expect(info).to.exist.and.to.be.a('function');
     expect(verbose).to.exist.and.to.be.a('function');
     expect(debug).to.exist.and.to.be.a('function');
     expect(silly).to.exist.and.to.be.a('function');
+  });
+
+  it('should create logger instance', () => {
+    const client = createLogger();
+    expect(client).to.exist;
+  });
+
+  it('should not re-create logger instance', () => {
+    const first = createLogger();
+    const second = createLogger();
+    expect(first).to.exist;
+    expect(second).to.exist;
+    expect(first.id === second.id).to.be.true;
+  });
+
+  it('should dispose logger instance', () => {
+    const logger = createLogger();
+    expect(logger).to.exist;
+    const disposed = disposeLogger();
+    expect(disposed).to.not.exist;
+  });
+
+  after(() => {
+    disposeLogger();
   });
 });
