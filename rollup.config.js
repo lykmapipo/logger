637de623a6c538659c5d54cbd0534222dbfd517c
lykmapipo <lallyelias87@gmail.com>
2019-04-10 15:04:38 +0300

    chore: initialize & setup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    external: ['lodash','@lykmapipo/env','@lykmapipo/common','winston'],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        interop: false,
+        esModule: false,
+        preferConst: true,
+        strict: true,
+      },
+      { file: pkg.module, format: 'es' },
+    ],
+  },
+];
